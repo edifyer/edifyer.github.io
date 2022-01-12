fd897f2014d594f5925903d2425d071df60a33d7
Aashish Chakravarty <chakravartyaashish@gmail.com>
2022-01-12 23:07:06 +0530

    Contact us form completed

--- next.config.js
+++ next.config.js
@@ -6,6 +6,11 @@ const nextConfiguration = {
     disableStaticImages: true,
   },
   target: 'serverless', //will output independent pages that don't require a monolithic server. It's only compatible with next start or Serverless deployment platforms (like ZEIT Now) — you cannot use the custom server API.
+  env: {
+    SERVICE_ID: process.env.SERVICE_ID,
+    TEMPLATE_ID: process.env.TEMPLATE_ID,
+    USER_ID: process.env.USER_ID,
+  },
 };
 
 module.exports = withPlugins([optimizedImages], nextConfiguration);
--- src/components/contact-us-form.js
+++ src/components/contact-us-form.js
@@ -1,22 +1,53 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
 import { jsx, Label, Flex, Input, Button } from 'theme-ui';
+import emailjs from '@emailjs/browser';
 
 const ContactUsForm = ({ buttonLabel, ...props }) => {
+  const onSubmit = (event) => {
+    event.preventDefault(); // Prevents default refresh by the browser
+
+    const data = {
+      name: event.target.name.value,
+      email: event.target.email.value,
+      message: event.target.message.value,
+    };
+
+    emailjs
+      .send(
+        process.env.SERVICE_ID,
+        process.env.TEMPLATE_ID,
+        data,
+        process.env.USER_ID
+      )
+      .then(
+        (response) => {
+          alert("Message Sent, I'll get back to you shortly");
+        },
+        (err) => {
+          alert('An error occured, Plese try again');
+        }
+      );
+  };
   return (
-    <Flex as="form" sx={styles.form} {...props}>
+    <Flex as="form" sx={styles.form} {...props} onSubmit={onSubmit}>
       <Label htmlFor="name" variant="styles.srOnly">
         Name
       </Label>
-      <Input type="text" id="name" placeholder="Enter Name" />
+      <Input type="text" id="name" placeholder="Enter Name" required />
       <Label htmlFor="email" variant="styles.srOnly">
         Email
       </Label>
-      <Input type="email" id="email" placeholder="Enter Email address" />
+      <Input
+        type="email"
+        id="email"
+        placeholder="Enter Email address"
+        required
+      />
       <Label htmlFor="message" variant="styles.srOnly">
         Message
       </Label>
-      <Input type="message" id="message" placeholder="Enter Message" />
+      <Input type="message" id="message" placeholder="Enter Message" required />
       <Button>{buttonLabel ?? 'Submit'}</Button>
     </Flex>
   );
@@ -33,6 +64,7 @@ const styles = {
       height: 'auto',
       width: 'auto',
       ml: [3],
+      my: [2],
     },
     button: {
       ml: [3],
